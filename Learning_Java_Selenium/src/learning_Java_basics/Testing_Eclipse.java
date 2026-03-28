2b2fb9eed4c2cc78e6d0505b04c6ac100dbfc3a1
Ankit Rautela <ankitrautelaar@gmail.com>
2026-03-28 18:18:25 +0530

    Testing eclipse first time

---
+++ Learning_Java_Selenium/src/learning_Java_basics/Testing_Eclipse.java
@@ -0,0 +1,10 @@
+package learning_Java_basics;
+
+public class Testing_Eclipse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+	}
+
+}
